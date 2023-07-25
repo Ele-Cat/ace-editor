59469faa2e4d622da04cfc745c52ea77c4498024
lijiamao <951572198@qq.com>
2023-07-25 17:38:40 +0800

    推送dist

--- src/components/Normal.vue
+++ src/components/Normal.vue
@@ -1,12 +1,33 @@
 <template>
   <div>
-    Normal
+    <editor v-model="content" @init="editorInit" lang="html" theme="chrome" width="100%" height="500"></editor>
+    <pre>
+      {{ content }}
+    </pre>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Normal"
+  name: "Normal",
+  data() {
+    return {
+      content: "",
+    }
+  },
+  components: {
+    editor: require('vue2-ace-editor'),
+  },
+  methods: {
+    editorInit: function () {
+      require('brace/ext/language_tools') //language extension prerequsite...
+      require('brace/mode/html')
+      require('brace/mode/javascript')    //language
+      require('brace/mode/less')
+      require('brace/theme/chrome')
+      require('brace/snippets/javascript') //snippet
+    }
+  },
 }
 </script>
 
