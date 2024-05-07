5f7130b071331033b39eb219e3bcda914ca0e195
lijiamao <951572198@qq.com>
2024-05-07 08:41:54 +0800

    修复打包

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   productionSourceMap: false,
-  // 用来在gitee page中配置多项目，无视即可
-  // publicPath: "https://ele-cat.gitee.io/ace-editor",
+  publicPath: "./",
   devServer: {
     port: 9527,
     open: true,
